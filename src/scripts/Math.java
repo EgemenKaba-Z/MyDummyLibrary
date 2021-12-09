5a6efc52cf6ac1e59b800f0d8c7f589bc49ab2bd
Kaba, Egemen <egemen.kaba@zuehlke.com>
2021-12-09 16:03:04 +0100

    double sum

--- src/scripts/Math.java
+++ src/scripts/Math.java
@@ -7,5 +7,9 @@ public class Math {
 	public static int sum(int...ints) {
 		return Arrays.stream(ints).sum();
 	}
+	
+	public static double sum(double...doubles) {
+		return Arrays.stream(doubles).sum();
+	}
 
 }
